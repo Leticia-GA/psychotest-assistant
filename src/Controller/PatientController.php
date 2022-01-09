268ec17e8e57280e547838d06f0fe1fb3d3ca7f2
Leticia <gonzalezalvarezleticia@gmail.com>
2022-01-09 21:33:45 +0100

    patients form

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Patient;
+use App\Form\Type\PatientType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,4 +29,18 @@ class PatientController extends AbstractController
 
         return $this->render('patients/list.html.twig', ['patients' => $patients]);
     }
+
+    /**
+     * @Route("/patients/new")
+     */
+    public function new(Request $request): Response
+    {
+        $patient = new Patient('', '', '', '');
+        $form = $this->createForm(PatientType::class, $patient);
+
+        return $this->renderForm('patients/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+    
 }
---
+++ src/Form/Type/PatientType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Patient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PatientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nombre'])
+            ->add('surnames', TextType::class, ['label' => 'Apellidos'])
+            ->add('phoneNumber', TextType::class, ['label' => 'Teléfono'])
+            ->add('email', TextType::class, ['label' => 'Email'])
+            ->add('save', SubmitType::class, ['label' => 'Añadir'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Patient::class,
+        ]);
+    }
+}
